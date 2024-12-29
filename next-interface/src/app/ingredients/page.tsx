04f05737d04aa5ff1112c39251490edec58a1d44
Tyree Carpenter <carpentertd@vcu.edu>
2024-12-28 21:43:10 -0500

    Test Commit from Git-ui

--- next-interface/src/app/ingredients/page.tsx
+++ next-interface/src/app/ingredients/page.tsx
@@ -1,11 +1,10 @@
-import { IngredientCard, IngredientCards } from "@/components/IngredientCard";
+import { IngredientCards } from "@/components/IngredientCard";
 import api from "@/lib/api";
 
 export default async function IngredientsPage() {
   return (
     <div className="flex w-screen h-screen items-center justify-center">
       <IngredientCards ingredients={await api.ingredients.fetchAll()} />
-      <IngredientCard ingredient={await api.ingredients.fetchById(6)} />
     </div>
   );
 }
--- next-interface/src/app/recipes/page.tsx
+++ next-interface/src/app/recipes/page.tsx
@@ -1,11 +1,10 @@
-import { RecipeCard, RecipeCards } from "@/components/RecipeCard";
+import { RecipeCards } from "@/components/RecipeCard";
 import api from "@/lib/api";
 
 export default async function RecipesPage() {
   return (
     <div className="flex w-screen h-screen items-center justify-center">
       <RecipeCards recipes={await api.recipes.fetchAll()} />
-      <RecipeCard recipe={await api.recipes.fetchById(6)} />
     </div>
   );
 }
