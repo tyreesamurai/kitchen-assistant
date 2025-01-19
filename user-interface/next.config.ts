065d03e2dc3224ba8c27ff275f0cc1f0652b0cec
Tyree Carpenter <carpentertd@vcu.edu>
2025-01-19 17:37:58 -0500

    Changed for deployment?

--- user-interface/next.config.ts
+++ user-interface/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Required for static exports
+  basePath: "/<repository-name>", // Replace <repository-name> with your GitHub repo name
+  trailingSlash: true, // Helps with static hosting
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
