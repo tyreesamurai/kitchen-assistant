4e69bf5da7b8d81b81d17bc241e7969d435c7485
Tyree Carpenter <carpentertd@vcu.edu>
2025-01-19 18:03:16 -0500

    I'm getting tired

--- user-interface/next.config.ts
+++ user-interface/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Required for static exports
-  basePath: "/<repository-name>", // Replace <repository-name> with your GitHub repo name
+  basePath: "/kitchen-assistant", // Replace <repository-name> with your GitHub repo name
   trailingSlash: true, // Helps with static hosting
 };
 
