86a526a772fb267850e8fc4cf0183e0a1068abb0
Tyree Carpenter <carpentertd@vcu.edu>
2025-01-19 16:30:15 -0500

    Got rid of what was messing up the build

--- user-interface/src/app/recipes/page.tsx
+++
@@ -1,7 +0,0 @@
-import RecipeCards from "@/components/RecipeCards";
-import api from "@/lib/api";
-
-export default async function RecipesPage() {
-  const recipes = await api.recipes.fetchAll();
-  return <RecipeCards recipes={recipes} />;
-}
