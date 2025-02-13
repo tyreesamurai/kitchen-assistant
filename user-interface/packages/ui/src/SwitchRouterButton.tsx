715b62da3a92a4329078596bd1c2210f86bb3f7a
Tyree Carpenter <carpentertd@vcu.edu>
2025-02-12 21:09:44 -0500

    Starting UI over from scratch I guess?

--- user-interface/packages/ui/src/SwitchRouterButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Anchor, Button } from 'tamagui'
-
-export const SwitchRouterButton = ({ pagesMode = false }: { pagesMode?: boolean }) => {
-  return (
-    <Anchor text="center" color="$color12" href={pagesMode ? '/' : '/pages-example'}>
-      <Button>Change router: {pagesMode ? 'pages' : 'app'}</Button>
-    </Anchor>
-  )
-}
