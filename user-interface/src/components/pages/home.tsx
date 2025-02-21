49291794096c8c28757cda7349dce5cca4cea8be
Tyree Carpenter <carpentertd@vcu.edu>
2025-02-20 21:00:01 -0500

    Name changes

--- user-interface/src/components/pages/home.tsx
+++ user-interface/src/components/pages/home.tsx
@@ -1,8 +1,8 @@
-import RecipePage from "@/components/pages/RecipePage";
+import RecipePage from "@/components/pages/recipe";
 import api from "@/lib/api";
 
 export default async function HomePage() {
-  const recipes = await api.recipes.getAll();
+  const recipes = await api.recipes.fetchAll();
   return (
     <>
       <RecipePage recipes={recipes} />
